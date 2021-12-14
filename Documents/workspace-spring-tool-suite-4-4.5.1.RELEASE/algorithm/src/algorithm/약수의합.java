ea3f22fc295f6380597f5bac112ee6e2d65a36d1
chohyeongshin <hgsin0102@gmail.com>
2021-12-14 18:06:40 +0900

    문제풀이 12/13

---
+++ Documents/workspace-spring-tool-suite-4-4.5.1.RELEASE/algorithm/src/algorithm/약수의합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+
+        for(int i = 1; i <= num/2; i++){
+            if(num%i == 0) answer += i;
+        }
+
+        return answer + num;
+    }
+}
